417191c3e3d28188bc575f0c9112b63e981902a8
fpacaud <francoispacaud8@gmail.com>
2022-05-04 11:14:41 -0500

    update MPI GPU launch script

--- test/mpi_optim_gpu.jl
+++ test/mpi_optim_gpu.jl
@@ -10,11 +10,13 @@ using NLPModels
 using MadNLP
 using MadNLPGPU
 
+using SparseArrays
 using CUDA
 using CUDAKernels
 
 # Load GPU utils
 include("/home/fpacaud/exa/Argos/test/cusolver.jl")
+
 # We are living at the edge
 CUDA.allowscalar(false)
 N_GPU = 2
@@ -64,6 +66,7 @@ nnzj = NLPModels.get_nnzj(blk)
 jac = zeros(nnzj)
 NLPModels.jac_coord!(blk, x0, jac)
 
+
 #=
     Step 2: Launch optimization
 =#
@@ -71,18 +74,6 @@ NLPModels.jac_coord!(blk, x0, jac)
 linear_solver = MadNLPLapackGPU
 verbose = is_master ? MadNLP.DEBUG : MadNLP.ERROR
 
-options = Dict{Symbol, Any}(
-    :tol=>1e-5,
-    :max_iter=>max_iter,
-    :nlp_scaling=>scaling,
-    :print_level=>verbose,
-    :linear_solver=>linear_solver,
-    :lapackgpu_algorithm=>MadNLPLapackGPU.CHOLESKY,
-    :dual_initialized=>true,
-)
-
-madopt = MadNLP.Options(linear_solver=linear_solver)
-MadNLP.set_options!(madopt, options, Dict())
 
 T = Float64
 VI = CuVector{Int}
@@ -90,7 +81,23 @@ VT = CuVector{T}
 MT = CuMatrix{T}
 
 KKT = SchurDecomposition.SchurKKTSystem{T, VI, VT, MT}
-ipp = MadNLP.InteriorPointSolver{KKT}(blk, madopt; option_linear_solver=options)
-MadNLP.optimize!(ipp)
+
+ntrials = 10
+for i in 1:ntrials
+    options = Dict{Symbol, Any}(
+        :tol=>1e-5,
+        :max_iter=>max_iter,
+        :nlp_scaling=>scaling,
+        :print_level=>verbose,
+        :linear_solver=>linear_solver,
+        :lapackgpu_algorithm=>MadNLPLapackGPU.CHOLESKY,
+        :dual_initialized=>true,
+    )
+    madopt = MadNLP.Options(linear_solver=linear_solver)
+    MadNLP.set_options!(madopt, copy(options), Dict())
+    ipp = MadNLP.InteriorPointSolver{KKT}(blk, madopt; option_linear_solver=options)
+    MadNLP.optimize!(ipp)
+end
 
 MPI.Finalize()
+
